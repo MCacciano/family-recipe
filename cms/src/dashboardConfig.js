47046e2a2cc05bf03243f21d415f1478307630e6
mcacciano <michael.cacciano@gmail.com>
2020-04-07 23:44:47 -0400

    setting up hosting for sanity studio

---
+++ cms/src/dashboardConfig.js
@@ -0,0 +1,19 @@
+export default {
+  widgets: [
+    {
+      name: 'Netlify',
+      options: {
+        title: 'Family Recipe',
+        sites: [
+          {
+            apiId:
+              'skDFQ8onFLI05HXyMuEof65O0RJzkOXRQ5TVcXqTENWIbJeYZPp6XMwXNR2j2bu1DzgoTtTrn1VpFn9FnUm1xsrRhbwWoBk5XY6YNiOqahB66djdRhu3uIP5XecyDBU8H4GEDFNtnHG9adCPAJk6bp25W8ZDCBfHENkt3piOLAI9kAEFbEOV',
+            buildHookId:
+              'https://api.netlify.com/build_hooks/5e8d398d44d11449b0e93adf',
+            name: 'family-recipe',
+          },
+        ],
+      },
+    },
+  ],
+};
