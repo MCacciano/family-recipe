97f0ae49ba5ea6cb611ecefa80ca3365144b8941
mcacciano <michael.cacciano@vmlyr.com>
2020-04-11 11:04:14 -0400

    messing with logo for sanity studio

---
+++ cms/src/components/dashboard/CustomLogo.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './logo.module.css';
+
+const CustomLogo = () => <img className={styles.customLogo} src='/static/salty.jpg' alt='Family Recipe Logo' />;
+
+export default CustomLogo;
